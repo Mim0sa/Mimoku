5b199393ed396d36815aacd039165c3a11606e31
Mim0sa <563576965@qq.com>
2021-01-15 17:32:42 +0800

    add filter extension

--- Mimoku_Example/Mimoku_Example/ViewController.swift
+++ Mimoku_Example/Mimoku_Example/ViewController.swift
@@ -12,7 +12,52 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        createGrid()
+        
+        
+    }
+    
+    func filter() {
+        let edge = (UIScreen.main.bounds.height - 400) / 5
+        
+        for i in 0...4 {
+            let imageView = UIImageView(image: #imageLiteral(resourceName: "demo"))
+            imageView.frame.size = CGSize(width: edge, height: edge)
+            imageView.contentMode = .scaleAspectFill
+            imageView.center = CGPoint(x: 50, y: 200 + edge / 2 + CGFloat(i) * edge)
+            view.addSubview(imageView)
+            let effect = UIImage.buildInFilterEffect[i]
+            imageView.image = #imageLiteral(resourceName: "demo").withBuildInFilter(filterName: effect.0, parameter: effect.1)
+        }
+        
+        for i in 5...9 {
+            let imageView = UIImageView(image: #imageLiteral(resourceName: "demo"))
+            imageView.frame.size = CGSize(width: edge, height: edge)
+            imageView.contentMode = .scaleAspectFill
+            imageView.center = CGPoint(x: 150, y: 200 + edge / 2 + CGFloat(i - 5) * edge)
+            view.addSubview(imageView)
+            let effect = UIImage.buildInFilterEffect[i]
+            imageView.image = #imageLiteral(resourceName: "demo").withBuildInFilter(filterName: effect.0, parameter: effect.1)
+        }
+        
+        for i in 10...14 {
+            let imageView = UIImageView(image: #imageLiteral(resourceName: "demo"))
+            imageView.frame.size = CGSize(width: edge, height: edge)
+            imageView.contentMode = .scaleAspectFill
+            imageView.center = CGPoint(x: 250, y: 200 + edge / 2 + CGFloat(i - 10) * edge)
+            view.addSubview(imageView)
+            let effect = UIImage.buildInFilterEffect[i]
+            imageView.image = #imageLiteral(resourceName: "demo").withBuildInFilter(filterName: effect.0, parameter: effect.1)
+        }
+        
+        for i in 15...19 {
+            let imageView = UIImageView(image: #imageLiteral(resourceName: "demo"))
+            imageView.frame.size = CGSize(width: edge, height: edge)
+            imageView.contentMode = .scaleAspectFill
+            imageView.center = CGPoint(x: 350, y: 200 + edge / 2 + CGFloat(i - 15) * edge)
+            view.addSubview(imageView)
+            let effect = UIImage.buildInFilterEffect[i]
+            imageView.image = #imageLiteral(resourceName: "demo").withBuildInFilter(filterName: effect.0, parameter: effect.1)
+        }
     }
     
     func createGrid() {
